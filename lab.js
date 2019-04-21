46a0aed69269439846434b3e3f934be6fa59053b
Ange Pagel <inkandev@gmail.com>
2019-04-21 03:35:30 +0200

    Add OrbitControls

--- lab.js
+++ lab.js
@@ -1,5 +1,5 @@
 
-let renderer, scene, camera;
+let renderer, scene, camera, controls;
 let ambientLight, spotLight;
 let cubeGeometry, cubeMaterial, cubeMesh;
 let floorGeometry, floorMaterial, floorMesh;
@@ -20,9 +20,11 @@ function init() {
     
     /* Camera */
     camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.01, 1000);
-    camera.position.z = 5;
-    camera.position.y = 2;
-    camera.rotation.x = -0.1;
+    camera.position.z = 10;
+    camera.position.y = 5;
+
+    /* Controls */
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
 
     /* White ambient Light */
     ambientLight = new THREE.AmbientLight(0xfffff, 0.6);
